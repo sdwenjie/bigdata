27108cfa6e5a9b0d6da7dad5aac2540e5692ec54
jcao <jcao@coupang.com>
2015-11-12 16:13:20 +0800

    Add an file including some example data.

---
+++ src/jie/wen/cao/sort/ExampleData.java
@@ -0,0 +1,10 @@
+package jie.wen.cao.sort;
+
+public class ExampleData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am example data!");
+	}
+
+}
